8491e239aa4d32bf760b76c9f5af35403445a249
AdrianPerez2207 <aperezsanchez441@gmail.com>
2024-01-08 20:48:59 +0100

    primer

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Password p1 = new Password();
+        Password p2 = new Password("12345678");
+
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+    }
+}
