cd9e41d3033f88f15f4292f3dfc5ca3079b26fba
AdrianPerez2207 <aperezsanchez441@gmail.com>
2024-01-08 20:49:34 +0100

    primer

---
+++ src/Password.java
@@ -0,0 +1,23 @@
+public class Password {
+    int longitud;
+    String texto;
+    //Constructor
+
+    public Password() {
+
+    }
+
+    public Password(String texto) {
+        this.texto = texto;
+        this.longitud = texto.length();
+    }
+    //Método
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Password{");
+        sb.append("longitud=").append(longitud);
+        sb.append(", texto='").append(texto).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
